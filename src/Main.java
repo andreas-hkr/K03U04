321a3bad3bc8dd0a6ee9f94a42a353e35cb61eed
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-14 17:02:16 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Beräkna avståndet mellan två punkter (x1, y1) och (x2, y2)
+        // Formel: roten ur( (x1-x2)² + (y1-y2)² )
+
+
+    }
+}
